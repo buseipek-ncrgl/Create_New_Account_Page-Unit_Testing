583f5435a2eac78eb400d34fb7692d0c2fc55580
buseipek-ncrgl <ipekncrgl@gmail.com>
2025-05-05 21:11:48 +0300

    Add basic unit tests for Validator

---
+++ CreateAccountJUnit/src/test/java/com/account/ValidatorTest.java
@@ -0,0 +1,89 @@
+package com.account;
+
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ValidatorTest {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String dob;
+
+    @BeforeEach
+    void setUp() {
+        firstName = "Alex";
+        lastName = "Smith";
+        email = "alex@example.com";
+        password = "Abc123$#";
+        dob = "01/01/2000";
+        System.out.println("🧪 Test başladı");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("✅ Test bitti\n");
+    }
+
+    @Test
+    void testValidAccount() {
+        assertTrue(Validator.validateAccount(firstName, lastName, email, password, password, dob));
+    }
+
+    @Test
+    void testInvalidEmail() {
+        assertFalse(Validator.isValidEmail("alex.com"));
+    }
+
+    @Test
+    void testEmptyEmail() {
+        assertFalse(Validator.isValidEmail(""));
+    }
+
+    @Test
+    void testEmptyFirstName() {
+        assertFalse(Validator.validateAccount("", lastName, email, password, password, dob));
+    }
+    
+    @Test
+    void testSingleCharacterFirstName() {
+        assertFalse(Validator.isValidName("A"));
+    }
+
+    @Test
+    void testPasswordMismatch() {
+        assertFalse(Validator.passwordsMatch("Abc123$#", "Xyz789!@"));
+    }
+
+    
+
+    @Test
+    void testInvalidDOBFormat() {
+        assertFalse(Validator.isValidDOB("2000-12-01"));
+    }
+
+    @Test
+    void testInvalidDay_TooHigh() {
+        assertFalse(Validator.isValidDOB("32/01/2000"));
+    }
+
+    @Test
+    void testInvalidMonth() {
+        assertFalse(Validator.isValidDOB("15/13/2000"));
+    }
+
+    @Test
+    void testFebruary29_NonLeapYear() {
+        assertFalse(Validator.isValidDOB("29/02/2001"));
+    }
+
+    @Test
+    void testFebruary29_LeapYear() {
+        assertTrue(Validator.isValidDOB("29/02/2024"));
+    }
+
+}
